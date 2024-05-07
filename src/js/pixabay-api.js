0d618b1e4e9214d128856b024ba4f3ff50398171
Yuri <wall.mart.boss@proton.me>
2024-05-07 23:55:03 +0300

    1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,2 @@
+//Your API key: 43769580-78f5aea5f54664bb89b2b40f7
+// sample  https://pixabay.com/api/?key=43769580-78f5aea5f54664bb89b2b40f7&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true
