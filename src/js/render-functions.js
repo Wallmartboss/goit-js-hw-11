8eaf7a3caf344071186a351d215a2345dc368a38
Yuri <wall.mart.boss@proton.me>
2024-05-08 12:16:27 +0300

    5

---
+++ src/js/render-functions.js
@@ -0,0 +1,23 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
+import { images } from './pixabay-api.js';
+
+const gallery = document.querySelector('ul.gallery');
+
+const createElGallery = image => {
+  return `<li class="gallery-item">
+        <a class="gallery-link" href="${image.largeUrl}">
+        <img class="gallery-image"
+         src="${image.url}"
+         alt="${image.tags}"
+        />
+        </a>
+        </li>`;
+};
+const createGallery = images.map(image => createElGallery(image)).join('');
+gallery.innerHTML = createGallery;
+
