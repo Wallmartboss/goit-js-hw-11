0646ac37217b485aaad852f17edd3a181a496015
Yuri <wall.mart.boss@proton.me>
2024-05-08 23:45:21 +0300

    almost

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,114 +1,100 @@
-//Your API key: 43769580-78f5aea5f54664bb89b2b40f7
-// sample  https://pixabay.com/api/?key=43769580-78f5aea5f54664bb89b2b40f7&q=yellow+flowers&image_type=photo&orientation=horizontal&safesearch=true
-
-// const images = [
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
-//     description: 'Hokkaido Flower',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
-//     description: 'Container Haulage Freight',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
-//     description: 'Aerial Beach View',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
-//     description: 'Flower Blooms',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-//     description: 'Alpine Mountains',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
-//     description: 'Mountain Lake Sailing',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
-//     description: 'Alpine Spring Meadows',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
-//     description: 'Nature Landscape',
-//   },
-//   {
-//     preview:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
-//     original:
-//       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
-//     description: 'Lighthouse Coast Sea',
-//   },
-// ];
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-export let images;
-fetch("https://pixabay.com/api/?key=43769580-78f5aea5f54664bb89b2b40f7&q=sharks&image_type=photo&orientation=horizontal&safesearch=true")
 
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .then(data => {
-    // Обрабатываем полученные данные
-    console.log(data);
-       // Пример обработки данных: вывод первой картинки из результатов
-    if (data.hits.length > 0) {
-      images = [];
-      for (let i = 0; i < data.hits.length; i++) {
-        // images.push(data.hits[i]);
-       
-        let pic = {};
-        pic.url = data.hits[i].webformatURL;
-        pic.largeUrl = data.hits[i].largeImageURL;
-        pic.tags = data.hits[i].tags;
-        pic.likes = data.hits[i].likes;
-        pic.views = data.hits[i].views;
-        pic.comments = data.hits[i].comments;
-        pic.downloads = data.hits[i].downloads;
-        images.push(pic);
-      }
-      // console.log(images);
-             } else {
-            iziToast.error({
+// import { images } from './pixabay-api.js';
+// console.log(images);
+
+const form = document.querySelector('.search-form');
+const input = form.querySelector('input');
+let userData;
+let images = [];
+const gallery = document.querySelector('ul.gallery');
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const textInput = input.value.trim();
+
+  if (textInput === "") {
+    iziToast.warning({
+    title: 'Caution',
+    message: 'This field must be filled in',
+    position: 'topLeft',
+})      } else {
+    userData = textInput;
+    form.reset();
+    gallery.innerHTML = "";
+
+    const searchUrl = `https://pixabay.com/api/?key=43769580-78f5aea5f54664bb89b2b40f7&q=${userData}&image_type=photo&orientation=horizontal&safesearch=true`;
+
+    fetch(searchUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        return response.json();
+      })
+      .then(data => {
+        images = [];
+        if (data.hits.length > 0) {
+          for (let i = 0; i < data.hits.length; i++) {
+            let pic = {};
+            pic.url = data.hits[i].webformatURL;
+            pic.largeUrl = data.hits[i].largeImageURL;
+            pic.tags = data.hits[i].tags;
+            pic.likes = data.hits[i].likes;
+            pic.views = data.hits[i].views;
+            pic.comments = data.hits[i].comments;
+            pic.downloads = data.hits[i].downloads;
+            images.push(pic);
+          }
+          const createElGallery = image => {
+            return `<li class="gallery-item">
+              <a class="gallery-link" href="${image.largeUrl}">
+                <img class="gallery-image"
+                  src="${image.url}"
+                  alt="${image.tags}"
+                />
+              </a>
+              <div class="item-info">
+                <div class="item-data">
+              <p class="item-param">Likes</p>
+              <p class="item-counter">${image.likes}</p>
+              </div>
+              <div class="item-data">
+              <p class="item-param">Views</p>
+              <p class="item-counter">${image.views}</p>
+              </div>
+              <div class="item-data">
+              <p class="item-param">Comments</p>
+              <p class="item-counter">${image.comments}</p>
+              </div>
+              <div class="item-data">
+              <p class="item-param">Downloads</p>
+              <p class="item-counter">${image.downloads}</p>
+              </div>
+              </div>
+            </li>`;
+          };
+          const createGallery = images.map(image => createElGallery(image)).join('');
+          gallery.innerHTML = createGallery;
+          let lightbox = new SimpleLightbox('.gallery a', {
+            captionsData: 'alt',
+          })
+        } else {
+          iziToast.error({
             title: 'Error',
             message: `Sorry, there are no images matching your search query. Please try again!`,
             position: 'topRight',
-           });;
-    }
-  })
-  .catch(error => {
-    console.error('Виникла помилка ', error);
-  });
-  
-    
+          });
+        }
+      })
+      .catch(error => {
+        console.error('Виникла помилка: ', error);
+      });
+  }
+}
+
+form.addEventListener("submit", handleSubmit);
--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 // import { data } from './js/pixabay-api.js';
 
+// const form = document.querySelector('.search-form');
+// // const searchString = document.querySelector(input);
+
+// form.addEventListener('submit', event => {
+//   event.preventDefault();
+//     const filledData = event.target.value;
+//     export default filledData;
+//     console.log(filledData);
+//   if (filledData == null) {
+//     alert(`Please begin filling form's fields`);
+//     return;
+//   } 
+//       form.reset();
+  
+// });
 
 
